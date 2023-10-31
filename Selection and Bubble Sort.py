fe9027f4d4b97bf50f017d51b40c7a1dfae81f7d
Divyesh Mali <125090897+divyesh-mali@users.noreply.github.com>
2023-10-31 11:34:26 +0530

    Fix the selection sort

--- Selection and Bubble Sort.py
+++ Selection and Bubble Sort.py
@@ -1,3 +1,5 @@
+# FIX THE SELECTION SORT !!!!!!!!!
+
 list = []
 
 n = int(input("Enter number of elements: "))
@@ -8,21 +10,27 @@ for i in range(n):
 
 print("\n", list)
 
-
-# FIX THIS SORT
 def selection_sort(list, n):
-    for i in range(n-1):
+    for i in range(n):
         min_index = i
 
         for j in range(i+1, n):
-            if list[i] < list[min_index]:
+            if list[j] < list[min_index]:
                 min_index = j
 
-            #list[i], list[min_index] = list[min_index], list[i]
-            list[i] = list[min_index]
-            list[min_index] = list [i]
+            list[i], list[min_index] = list[min_index], list[i]
+            # list[i] = list[min_index]
+            # list[min_index] = list [i]
     
     return list
+
+def bubble_sort(list, n):
+    for i in range(n):
+        for j in range(n-i-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    return list
+
 
 print("Enter your choice: ")
 while True:
@@ -30,7 +38,13 @@ while True:
     choice = int(input("Enter the choice: "))
 
     if choice == 1:
-        selection_sort(n, list)
+        print(selection_sort(n, list))
+
+    if choice == 2:
+        print(bubble_sort(list, n))
     
     elif choice == 3:
         break
+
+    else:
+        print("Invalid choice entered !!")
