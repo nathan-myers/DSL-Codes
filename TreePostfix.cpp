362d691185bc18b1e332790a0803169166efb0fa
nathan-myers <158767745+nathan-myers@users.noreply.github.com>
2024-03-12 12:16:28 +0530

    Create TreePostfix.cpp

---
+++ TreePostfix.cpp
@@ -0,0 +1,79 @@
+// Check code in photos
+
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+
+struct Node
+{
+    Node *left;
+    Node *right;
+    char data;
+};
+
+class Tree
+{   
+    char prefix[20];
+    
+    public:
+    Node *top;
+
+    void expression(prefix[])
+    {
+        Stack s;
+        int length, i;
+        Node *top, *t1, *t2;
+        len
+
+    }
+
+    void display(Node *)
+    {
+
+    }
+
+    void displayPostOrder(Node *)
+    {
+
+    }
+
+    void deleteNode(Node *)
+    {
+
+    }
+};
+
+class Stack
+{
+    public:
+    Node *data[20];
+    int top;
+
+    Stack()
+    {
+        top = -1;
+    }
+
+    void push(Node *p)
+    {
+
+    }
+
+    Node *pop()
+    {
+
+    }
+
+    int empty()
+    {
+
+    }
+
+
+    
+}
+
+int main()
+{
+
+}
